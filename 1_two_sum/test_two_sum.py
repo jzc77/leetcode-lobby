66e6d90771c4824614d8e65b059b83d4e62ef9d4
JazC <jchan7713@gmail.com>
2023-01-08 23:48:35 -0800

    Add unit tests and notes

--- 1_two_sum/test_two_sum.py
+++ 1_two_sum/test_two_sum.py
@@ -1,5 +1,6 @@
 import unittest
-from two_sum import two_sum
+import itertools
+from two_sum import two_sum, two_sum_approach_2
 
 """
 Run all tests in the command line with the command:
@@ -26,3 +27,20 @@ class Test_two_sum(unittest.TestCase):
   def test_contains_negative_integers(self):
     # Tests arrays that contains a negative integer
     self.assertEqual(two_sum([3, -10, 2, 19], 9), [1, 3])
+
+class Test_two_sum_approach_2(unittest.TestCase):
+  def test_contains_positive_integers_in_order(self):
+    # Tests arrays that contains positive integers in order
+    self.assertEqual(two_sum_approach_2([2, 7, 11, 15], 9), [1, 0])
+
+  def test_contains_positive_integers_not_in_order(self):
+  # Tests arrays that contains positive integers not in order
+    self.assertEqual(two_sum_approach_2([3,2,4], 6), [2, 1])
+
+  def test_contains_same_positive_integers(self):
+    # Tests arrays that contain the same positive integers
+    self.assertEqual(two_sum_approach_2([3,3], 6), [1, 0])
+
+  def test_contains_negative_integers(self):
+    # Tests arrays that contains a negative integer
+    self.assertEqual(two_sum_approach_2([3, -10, 2, 19], 9), [3, 1])
--- 1_two_sum/two_sum.py
+++ 1_two_sum/two_sum.py
@@ -11,45 +11,42 @@ Thought process:
 -If there is no match, repeat the above steps with the next number in the list
 -Continue until an answer is found (the question says that there is exactly one solution)
 '''
-# def two_sum(nums: List[int], target: int) -> List[int]:
-#   output_list = []
+def two_sum(nums: List[int], target: int) -> List[int]:
+  output_list = []
   
-#   for index_first_number, first_number in enumerate(nums):
-#     new_target = target - first_number  # e.g. 9-2=7
-#     for index_second_number, second_number in enumerate(nums[1:], start=1):  # [1:] means start the list at index 1. "start=1" means get the index number of the second list element
-#       if second_number == new_target:
-#         output_list.append(index_first_number)
-#         output_list.append(index_second_number)
-#         return output_list
+  for index_first_number, first_number in enumerate(nums):
+    new_target = target - first_number  # e.g. 9-2=7
+    for index_second_number, second_number in enumerate(nums[1:], start=1):  # [1:] means start the list at index 1. "start=1" means get the index number of the second list element
+      if second_number == new_target:
+        output_list.append(index_first_number)
+        output_list.append(index_second_number)
+        return output_list
 
-# #two_sum([2,7,11,15], 9)
-# #two_sum([3,3], 6)
-# two_sum([3, -10, 2, 19], 9)
+#two_sum([2,7,11,15], 9)
+#two_sum([3,3], 6)
+two_sum([3, -10, 2, 19], 9)
 
 '''
 Thought process:
 (Dictionary)
 -Iterate through the list of numbers and store the number as keys in a dictionary
--Also, store the number's index as values
 -Going through the list of numbers, check to see if the target minus current number equals a key in the dictionary
 -If so, stop the iteration and return current number's index AND the key's value (which is the index of the number 
 (in which the number/key adds to the current number to equal the target)
+-If not, store the number's index as values, and continue iterating
 '''
 def two_sum_approach_2(nums: List[int], target: int) -> List[int]:
-  nums_difference_dict = {}
+  nums_index_dict = {}
   
   for index, num in enumerate(nums):
     potential_key = target - num
-    if potential_key in nums_difference_dict.keys():
-      print(nums_difference_dict)
-      return([index, nums_difference_dict[potential_key]])
-    nums_difference_dict[num] = index
+    if potential_key in nums_index_dict.keys():
+      return([index, nums_index_dict[potential_key]])
+    nums_index_dict[num] = index
+    # need to move this down here or else it will cause accidental num (e.g. 3 in index 0) and the added potential_key
+    # (e.g. the same 3, with corresponding value of 0) to add up to the target (e.g. 6), returning the incorrect output (e.g. [0, 0])
 
-#print(two_sum_approach_2([2,7,11,15], 9))      # should contain [1,0], correct
-print(two_sum_approach_2([3,2,4], 6))           # should contain [1,2]
-#print(two_sum_approach_2([3,3], 6))            # should contain [0,1]
-#print(two_sum_approach_2([3, -10, 2, 19], 9))  # should contain [1,3], correct
-
+# Example dictionaries:
 # dict = {
 #   3: 0,
 #   2: 1,
@@ -61,11 +58,4 @@ print(two_sum_approach_2([3,2,4], 6))           # should contain [1,2]
 #   -10: 1,
 #   2: 2,
 #   19: 3,
-# }
-
-# dict = {
-#   3: 6,
-#   -10: 19,
-#   2: 7,
-#   19: -10,
 # }
