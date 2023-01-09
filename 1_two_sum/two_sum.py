739447bf0bbc41a9e579a9a123a8025927d2dd6e
JazC <jchan7713@gmail.com>
2023-01-08 22:02:03 -0800

    Add semi-correct solution to two_sum

--- 1_two_sum/two_sum.py
+++ 1_two_sum/two_sum.py
@@ -11,17 +11,59 @@ Thought process:
 -If there is no match, repeat the above steps with the next number in the list
 -Continue until an answer is found (the question says that there is exactly one solution)
 '''
-def two_sum(nums: List[int], target: int) -> List[int]:
-  output_list = []
+# def two_sum(nums: List[int], target: int) -> List[int]:
+#   output_list = []
   
-  for index_first_number, first_number in enumerate(nums):
-    new_target = target - first_number  # e.g. 9-2=7
-    for index_second_number, second_number in enumerate(nums[1:], start=1):  # [1:] means start the list at index 1. "start=1" means get the index number of the second list element
-      if second_number == new_target:
-        output_list.append(index_first_number)
-        output_list.append(index_second_number)
-        return output_list
+#   for index_first_number, first_number in enumerate(nums):
+#     new_target = target - first_number  # e.g. 9-2=7
+#     for index_second_number, second_number in enumerate(nums[1:], start=1):  # [1:] means start the list at index 1. "start=1" means get the index number of the second list element
+#       if second_number == new_target:
+#         output_list.append(index_first_number)
+#         output_list.append(index_second_number)
+#         return output_list
 
-#two_sum([2,7,11,15], 9)
-#two_sum([3,3], 6)
-two_sum([3, -10, 2, 19], 9)
+# #two_sum([2,7,11,15], 9)
+# #two_sum([3,3], 6)
+# two_sum([3, -10, 2, 19], 9)
+
+'''
+Thought process:
+(Dictionary)
+-Iterate through the list of numbers and store the number as keys in a dictionary
+-Also, store the number's index as values
+-Going through the list of numbers, check to see if the target minus current number equals a key in the dictionary
+-If so, stop the iteration and return current number's index AND the key's value (which is the index of the number 
+(in which the number/key adds to the current number to equal the target)
+'''
+def two_sum_approach_2(nums: List[int], target: int) -> List[int]:
+  nums_difference_dict = {}
+
+  for index, num in enumerate(nums):
+    nums_difference_dict[num] = index
+    potential_key = target - num
+    if potential_key in nums_difference_dict.keys():
+      return([index, nums_difference_dict[potential_key]])
+
+    # for every num, look it up in the dict so far
+    # if num in nums_difference_dict.values():
+    #   return [index, ]
+  #print(nums_difference_dict)
+
+#print(two_sum_approach_2([2,7,11,15], 9))
+print(two_sum_approach_2([3,2,4], 6))
+#print(two_sum_approach_2([3,3], 6))
+#$print(two_sum_approach_2([3, -10, 2, 19], 9))
+
+# dict = {
+#   3: 0,
+#   -10: 1,
+#   2: 2,
+#   19: 3,
+# }
+
+# dict = {
+#   3: 6,
+#   -10: 19,
+#   2: 7,
+#   19: -10,
+# }
