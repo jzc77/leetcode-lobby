8d80e1d95c7bf3b213c9206b4704ed77b3535b6e
JazC <jchan7713@gmail.com>
2023-02-03 23:15:11 -0800

    Solve 125_valid_palindrome

--- 125_valid_palindrome.py
+++ 125_valid_palindrome.py
@@ -6,6 +6,13 @@ https://leetcode.com/problems/valid-palindrome/
 Thought process:
 (Brute Force)
 -First, remove all non-alphanumeric characters from the string
+-Reverse the string
+-Compare original and the reversed string
+  -If match, then return true, it is a palindrome
+  -Else, return false (not a palindrome)
+
+Not needed...:
+-Make all characters lowercase, if they are letters
 -Compare the first and last characters of the string
   -If the characters are equal, move to the inner character from both sides
   -If the characters are not equal, return false (not a palindrome)
@@ -13,3 +20,14 @@ Thought process:
     -the indices are 1 number away from each other (even number of characters)(then return true, it is a palindrome) or
     -the indices are the same (odd number of characters)(then return true, it is a palindrome)
 '''
+def isPalindrome(input_string: str) -> bool:
+  alpha_num_string = "".join(char.lower() for char in input_string if char.isalnum())
+  alpha_num_string_reverse = alpha_num_string[::-1]
+
+  if alpha_num_string == alpha_num_string_reverse:
+    return True
+  else:
+    return False
+
+
+print(isPalindrome("A man, a plan, a canal: Panama"))
