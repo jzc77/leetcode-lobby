14256b5b152659613b4f1e9b1d072a5e5b6210d9
JazC <jchan7713@gmail.com>
2023-02-06 22:46:33 -0800

    Second approach for 125_valid_palindrome

--- 125_valid_palindrome.py
+++ 125_valid_palindrome.py
@@ -11,7 +11,7 @@ Thought process:
   -If match, then return true, it is a palindrome
   -Else, return false (not a palindrome)
 
-Not needed...:
+See second implementation:
 -Make all characters lowercase, if they are letters
 -Compare the first and last characters of the string
   -If the characters are equal, move to the inner character from both sides
@@ -22,7 +22,31 @@ Not needed...:
 '''
 # To run file: python 125_valid_palindrome.py
 # To run doctest: python -m doctest 125_valid_palindrome.py -v
-def isPalindrome(input_string: str) -> bool:
+# def isPalindrome(input_string: str) -> bool:
+#     """
+#     Given a string, see if it is a palindrome.
+
+#     :param: str
+#     :return: bool
+
+#     >>> isPalindrome("A man, a plan, a canal: Panama")
+#     True
+#     >>> isPalindrome("race a car")
+#     False
+#     >>> isPalindrome(" ")
+#     True
+#     """
+#     alpha_num_string = "".join(char.lower() for char in input_string if char.isalnum())
+#     alpha_num_string_reverse = alpha_num_string[::-1]
+
+#     if alpha_num_string == alpha_num_string_reverse:
+#       return True
+#     else:
+#       return False
+
+## TODO: Implement the other way to solve this question...
+## Note: need two functions
+def isPalindrome_usingPointers(input_string: str) -> bool:
     """
     Given a string, see if it is a palindrome.
 
@@ -36,12 +60,27 @@ def isPalindrome(input_string: str) -> bool:
     >>> isPalindrome(" ")
     True
     """
-    alpha_num_string = "".join(char.lower() for char in input_string if char.isalnum())
-    alpha_num_string_reverse = alpha_num_string[::-1]
+    left, right = 0, len(input_string) - 1
+    
+    while input_string[left] < input_string[right]:
+      if isAlphaNum(input_string[left]) and isAlphaNum(input_string[right]):  # both comparing characters are alphanumeric
+        if input_string[left] == input_string[right]:
+          left += 1
+          right -= 1
+        else:
+          return False  # once comparing characters are not matching, return false (the input string is not a palindrome)
+      else:
+        if isAlphaNum(input_string[left]) == False:
+          left += 1
+        if isAlphaNum(input_string[right]) == False:
+          right -= 1
+    return True
 
-    if alpha_num_string == alpha_num_string_reverse:
-      return True
-    else:
-      return False
 
-## TODO: Implement the other way to solve this question...
+def isAlphaNum(character: str):
+  isAlphaUpperCase = ord(character) > ord('A') and ord(character) < ord('Z')
+  isAlphaLowerCase = ord(character) > ord('a') and ord(character) < ord('z') 
+  isNum = ord(character) > ord('0') and ord(character) < ord('9') 
+  return isAlphaUpperCase or isAlphaLowerCase or isNum
+
+print(isPalindrome_usingPointers("race a car"))
