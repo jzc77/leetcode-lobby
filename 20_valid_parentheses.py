d6a817d643ebc58f41ab16896d5c893ba2bd0697
JazC <jchan7713@gmail.com>
2023-02-08 21:52:57 -0800

    Set up 20_valid_parentheses thought process

---
+++ 20_valid_parentheses.py
@@ -0,0 +1,34 @@
+"""
+https://leetcode.com/problems/valid-parentheses/
+"""
+
+'''
+Thought process:
+(Brute Force)
+-Iterate through each character in the string
+-Use 3 variables to keep track of each bracket type, with starting value of 0
+-For each open bracket type encountered, add 1 to the respective variable
+-For each close bracket type encountered, subtract 1 from the respective variable
+-After then end of the string iteration, add up the three variables
+    -If final sum is 0, then return true (string is valid)
+    -If final sum is not 0, then return false (string is not valid)
+'''
+# To run file: python 20_valid_parentheses.py
+# To run doctest: python -m doctest 20_valid_parentheses.py -v
+def isValid(input_string: str) -> bool:
+    """
+    Given a string of bracket characters, check if all the open brackets have closing brackets in the right order.
+
+    :param: str
+    :return: bool
+
+    >>> isValid("()")
+    True
+    >>> isValid("()[]{}")
+    True
+    >>> isValid("(]")
+    False
+    """
+
+if __name__ == "__main__":
+  isValid("()")
