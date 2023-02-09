f6643b40610a0ab090c93ddef41958d487c8e018
JazC <jchan7713@gmail.com>
2023-02-08 22:50:48 -0800

    Attempt at fixing ordering test case in 20_valid_parentheses

--- 20_valid_parentheses.py
+++ 20_valid_parentheses.py
@@ -47,6 +47,48 @@ def isValid(input_string: str) -> bool:
         bracket_3 -= 1
     return bracket_1 == bracket_2 == bracket_3 == 0
 
+'''
+Thought process:
+(Using stack)
+-Iterate through each character in the string
+-Use a list to keep track of open characters put into it
+-If a close bracket is encountered, the corresponding open bracket must be 'popped' off from the list
+-After then end of the string iteration, check if the list is empty
+    -If list is empty, then return true (string is valid)
+    -If list is not empty, then return false (string is not valid)
+-This process ensures ordering is correct
+'''
+def isValid_withOrdering(input_string: str) -> bool:
+    """
+    Given a string of bracket characters, check if all the open brackets have closing brackets in the right order.
+
+    :param: str
+    :return: bool
+
+    >>> isValid_withOrdering("()")
+    True
+    >>> isValid_withOrdering("()[]{}")
+    True
+    >>> isValid_withOrdering("(]")
+    False
+    >>> isValid_withOrdering("([)]")
+    False
+    """
+    brackets_list = []
+    open_brackets = ['(', '[', '{']
+    closed_brackets = [')', ']', '}']
+
+    for character in input_string:
+      if character in open_brackets:
+        brackets_list.append(character)
+      else:
+        if character == closed_brackets[open_brackets.index(character)]:
+          brackets_list.pop(character)
+        else:
+          return False
+    return True
+
 
 if __name__ == "__main__":
-  isValid("()")
+  # isValid("()")
+  isValid_withOrdering("{}]")
